520b9a3527c708f60c02c29c04b7cf6ee718fa9f
gugamaxara <gugamaxara@github.com>
2020-04-22 22:01:03 +0400

    changed classical Thread usage to ExecutorService

--- Trainings/src/lesson200421/ThreadStartExamples1.java
+++ Trainings/src/lesson200421/ThreadStartExamples1.java
@@ -1,29 +1,45 @@
 package lesson200421;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import utils.Utils;
+
 public class ThreadStartExamples1 {
 
 	public static void main(String[] args) {
-		System.out.println(Thread.currentThread());
-
-		Thread.currentThread().setName("MyThread");
-		Thread.currentThread().setPriority(7);
-		System.out.println(Thread.currentThread());
-
-		new Thread(() -> System.out.println(Thread.currentThread()))
-				.start();
-
-		new Thread(ThreadStartExamples1::cycle).start();
-	}
-
-	private static void cycle() {
-		while (true) {
-			System.out.println(" hi there");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		
+		while(true) {
+			executor.execute(() -> {
+				System.out.println("hi there");
+				Utils.pause(1000);
+			});
 		}
+		
+		
+//		System.out.println(Thread.currentThread());
+//
+//		Thread.currentThread().setName("MyThread");
+//		Thread.currentThread().setPriority(7);
+//		System.out.println(Thread.currentThread());
+//
+//		new Thread(() -> System.out.println(Thread.currentThread()))
+//				.start();
+//
+//		new Thread(ThreadStartExamples1::cycle).start();
 	}
+
+//	private static void cycle() {
+//		while (true) {
+//			System.out.println(" hi there");
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
 
 }
